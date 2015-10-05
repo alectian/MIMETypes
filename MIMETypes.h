e7adc4aa4becc0ddd03e0829a89c55f041813b44
Ken M. Haggerty <eurekavalleyco@gmail.com>
2015-10-05 14:51:54 -0400

    Adds MIME types

---
+++ MIMETypes.h
@@ -0,0 +1,62 @@
+//
+//  MIMETypes.h
+//  Kaiten
+//
+//  Created by Ken M. Haggerty on 10/5/15.
+//  Copyright © 2015 Eureka Valley Co. All rights reserved.
+//
+
+#pragma mark - // NOTES (Public) //
+
+#pragma mark - // IMPORTS (Public) //
+
+#pragma mark - // PROTOCOLS //
+
+#pragma mark - // DEFINITIONS (Public) //
+
+#define MIME_AUDIO_MPEG @"audio/mpeg"
+#define MIME_AUDIO_MPEG4 @"audio/mp4"
+
+#define MIME_CALENDAR_APPLE @"text/calendar"
+
+#define MIME_DOCUMENT_ADOBE_PHOTOSHOP @"image/vnd.adobe.photoshop"
+
+#define MIME_DOCUMENT_HYPERSTUDIO @"application/hyperstudio"
+
+#define MIME_DOCUMENT_MICROSOFT_EXCEL @"application/vnd.ms-excel"
+#define MIME_DOCUMENT_MICROSOFT_POWERPOINT @"application/vnd.ms-powerpoint"
+#define MIME_DOCUMENT_MICROSOFT_WORD @"application/msword"
+
+#define MIME_DOCUMENT_OPENDOCUMENT_PRESENTATION @"application/vnd.oasis.opendocument.presentation"
+#define MIME_DOCUMENT_OPENDOCUMENT_SPREADSHEET @"application/vnd.oasis.opendocument.spreadsheet"
+#define MIME_DOCUMENT_OPENDOCUMENT_TEXT @"application/vnd.oasis.opendocument.text"
+
+#define MIME_DOCUMENT_OPENOFFICE_CALC @"application/vnd.sun.xml.calc"
+#define MIME_DOCUMENT_OPENOFFICE_IMPRESS @"application/vnd.sun.xml.impress"
+#define MIME_DOCUMENT_OPENOFFICE_WRITER @"application/vnd.sun.xml.writer"
+
+#define MIME_DOCUMENT_PDF @"application/pdf"
+
+#define MIME_EBOOK_AMAZON_KINDLE @"application/vnd.amazon.ebook"
+
+#define MIME_IMAGE_PNG @"image/png"
+#define MIME_IMAGE_GIF @"image/gif"
+#define MIME_IMAGE_JPEG @"image/jpeg"
+#define MIME_IMAGE_TIFF @"image/tiff"
+
+#define MIME_TEXT_HTML @"text/html"
+#define MIME_TEXT_JAVASCRIPT @"application/javascript"
+#define MIME_TEXT_JSON @"application/json"
+#define MIME_TEXT_LATEX @"application/x-latex"
+#define MIME_TEXT_PLAIN @"text/plain"
+#define MIME_TEXT_RICH @"application/rtf"
+#define MIME_TEXT_TEX @"application/x-tex"
+#define MIME_TEXT_XML @"application/xml"
+
+#define MIME_VIDEO_H264 @"video/h264"
+#define MIME_VIDEO_JPG @"video/jpeg"
+#define MIME_VIDEO_MPEG @"video/mpeg"
+#define MIME_VIDEO_MPEG4 @"video/mp4"
+#define MIME_VIDEO_QUICKTIME @"video/quicktime"
+
+#define MIME_ZIP @"application/zip"
